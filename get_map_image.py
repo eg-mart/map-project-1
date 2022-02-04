5c7ff52e4f486c10daa38e1e3ce33f12bdd28474
lava99 <oleg_bol05.05@mail.ru>
2022-02-04 18:49:02 +0400

    image using API

---
+++ get_map_image.py
@@ -0,0 +1,40 @@
+import requests
+from PyQt5.QtCore import QByteArray
+
+
+def get_map_image(toponym_to_find):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        # обработка ошибочной ситуации
+        pass
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    # Координаты центра топонима:
+    toponym_coodrinates = toponym["Point"]["pos"]
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+    delta = "0.05"
+
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        "ll": ",".join([toponym_longitude, toponym_lattitude]),
+        "spn": ",".join([delta, delta]),
+        "l": "map"
+    }
+
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=map_params)
+    return QByteArray(response.content)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PyQt5 import QtGui, QtCore, QtWidgets
+from get_map_image import get_map_image
 import sys
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     tmp = QtGui.QPixmap()
-    QtGui.QPixmap.load(tmp, 'map.png')
+    tmp.loadFromData(get_map_image(input()))
     scene = QtWidgets.QGraphicsScene()
     scene.addPixmap(tmp)
     view = QtWidgets.QGraphicsView(scene)
